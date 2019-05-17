deb31524a861380bf9ba259e35e283568fbc6036
Sait ORHAN <saitorhan@outlook.com>
2019-05-18 00:39:45 +0300

    instance state

--- app/src/main/java/com/saitorhan/instancestate/MainActivity.java
+++ app/src/main/java/com/saitorhan/instancestate/MainActivity.java
@@ -1,13 +1,43 @@
 package com.saitorhan.instancestate;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.labelText);
+    }
+
+    public void GetName(View view) {
+        String name = editText.getText().toString();
+        textView.setText(name);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("name", textView.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String name = savedInstanceState.getString("name");
+
+        textView.setText(name);
     }
 }
